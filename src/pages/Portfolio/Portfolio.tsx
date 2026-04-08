b6360570e1fc809ba45135e944e383c58e8ee7f3
FranGuh <pedicles62900@gmail.com>
2026-04-08 00:09:36 -0600

    Ajustado error de producción

--- src/pages/Portfolio/Portfolio.tsx
+++ src/pages/Portfolio/Portfolio.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { cvData } from '../../data/cvData';
 
 import AboutSection from '../../components/UI/AboutSection/AboutSection';
